e9600e8630e381e9d5c6459d88b4f26ec64c5bf7
AWS CDK Team <aws-cdk@amazon.com>
2021-03-26 01:00:58 +0000

    chore(release): v1.95.1-devpreview

--- awscdk/jsii/jsii.go
+++ awscdk/jsii/jsii.go
@@ -12,7 +12,7 @@ import (
 	constructs "github.com/aws/constructs-go/constructs/v3/jsii"
 )
 
-//go:embed monocdk-1.95.0.tgz
+//go:embed monocdk-1.95.1.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -22,5 +22,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("monocdk", "1.95.0", tarball)
+	_jsii_.Load("monocdk", "1.95.1", tarball)
 }
