194c06a7997e5ced4fd5e80bee17bf4a094a165d
AWS CDK Team <aws-cdk@amazon.com>
2021-10-22 13:48:50 +0000

    chore(release): awscdk@v2.0.0-rc.25  awscdkamplifyalpha@v2.0.0-alpha.2  awscdkapigatewayalpha/v2@v2.0.0-alpha.2  awscdkapigatewayauthorizersalpha/v2@v2.0.0-alpha.2  awscdkapigatewayintegrationsalpha/v2@v2.0.0-alpha.2  awscdkapprunneralpha@v2.0.0-alpha.2  awscdkappsyncalpha@v2.0.0-alpha.2  awscdkassertionsalpha@v2.0.0-alpha.2  awscdkbatchalpha@v2.0.0-alpha.2  awscdkcloud9alpha@v2.0.0-alpha.2  awscdkcodestaralpha@v2.0.0-alpha.2  awscdkgluealpha@v2.0.0-alpha.2  awscdkiotalpha@v2.0.0-alpha.2  awscdkivsalpha@v2.0.0-alpha.2  awscdkkinesisanalyticsflinkalpha@v2.0.0-alpha.2  awscdkkinesisfirehosealpha@v2.0.0-alpha.2  awscdkkinesisfirehosedestinationsalpha@v2.0.0-alpha.2  awscdklambdagoalpha@v2.0.0-alpha.2  awscdklambdapythonalpha@v2.0.0-alpha.2  awscdkmskalpha@v2.0.0-alpha.2  awscdkneptunealpha@v2.0.0-alpha.2  awscdkredshiftalpha@v2.0.0-alpha.2  awscdkroute53resolveralpha@v2.0.0-alpha.2  awscdkservicecatalogalpha@v2.0.0-alpha.2  awscdkservicecatalogappregistryalpha@v2.0.0-alpha.2  awscdksyntheticsalpha@v2.0.0-alpha.2

--- awscdkivsalpha/jsii/jsii.go
+++ awscdkivsalpha/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	constructs "github.com/aws/constructs-go/constructs/v10/jsii"
 )
 
-//go:embed aws-cdk-aws-ivs-alpha-2.0.0-alpha.1.tgz
+//go:embed aws-cdk-aws-ivs-alpha-2.0.0-alpha.2.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@aws-cdk/aws-ivs-alpha", "2.0.0-alpha.1", tarball)
+	_jsii_.Load("@aws-cdk/aws-ivs-alpha", "2.0.0-alpha.2", tarball)
 }
