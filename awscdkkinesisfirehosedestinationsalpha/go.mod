99d2129e59c19038b4b01960c09c9b21b4aa26b1
AWS CDK Team <aws-cdk@amazon.com>
2021-11-17 11:50:31 +0000

    chore(release): awscdk@v2.0.0-rc.30  awscdkamplifyalpha@v2.0.0-alpha.7  awscdkapigatewayv2alpha@v2.0.0-alpha.7  awscdkapigatewayv2authorizersalpha@v2.0.0-alpha.7  awscdkapigatewayv2integrationsalpha@v2.0.0-alpha.7  awscdkapprunneralpha@v2.0.0-alpha.7  awscdkappsyncalpha@v2.0.0-alpha.7  awscdkbatchalpha@v2.0.0-alpha.7  awscdkcloud9alpha@v2.0.0-alpha.7  awscdkcodestaralpha@v2.0.0-alpha.7  awscdkgluealpha@v2.0.0-alpha.7  awscdkiotactionsalpha@v2.0.0-alpha.7  awscdkiotalpha@v2.0.0-alpha.7  awscdkivsalpha@v2.0.0-alpha.7  awscdkkinesisanalyticsflinkalpha@v2.0.0-alpha.7  awscdkkinesisfirehosealpha@v2.0.0-alpha.7  awscdkkinesisfirehosedestinationsalpha@v2.0.0-alpha.7  awscdklambdagoalpha@v2.0.0-alpha.7  awscdklambdapythonalpha@v2.0.0-alpha.7  awscdkmskalpha@v2.0.0-alpha.7  awscdkneptunealpha@v2.0.0-alpha.7  awscdkredshiftalpha@v2.0.0-alpha.7  awscdkroute53resolveralpha@v2.0.0-alpha.7  awscdkservicecatalogalpha@v2.0.0-alpha.7  awscdkservicecatalogappregistryalpha@v2.0.0-alpha.7  awscdksyntheticsalpha@v2.0.0-alpha.7

--- awscdkkinesisfirehosedestinationsalpha/go.mod
+++ awscdkkinesisfirehosedestinationsalpha/go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/aws/jsii-runtime-go v1.42.0
-	github.com/aws/aws-cdk-go/awscdkkinesisfirehosealpha/v2 v2.0.0-alpha.6
-	github.com/aws/aws-cdk-go/awscdk/v2 v2.0.0-rc.29
+	github.com/aws/aws-cdk-go/awscdkkinesisfirehosealpha/v2 v2.0.0-alpha.7
+	github.com/aws/aws-cdk-go/awscdk/v2 v2.0.0-rc.30
 	github.com/aws/constructs-go/constructs/v10 v10.0.9
 )
--- awscdkmskalpha/jsii/jsii.go
+++ awscdkmskalpha/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	constructs "github.com/aws/constructs-go/constructs/v10/jsii"
 )
 
-//go:embed aws-cdk-aws-msk-alpha-2.0.0-alpha.6.tgz
+//go:embed aws-cdk-aws-msk-alpha-2.0.0-alpha.7.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@aws-cdk/aws-msk-alpha", "2.0.0-alpha.6", tarball)
+	_jsii_.Load("@aws-cdk/aws-msk-alpha", "2.0.0-alpha.7", tarball)
 }
