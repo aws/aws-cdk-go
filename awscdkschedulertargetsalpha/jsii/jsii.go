312f900877f05a8bb082a426bd37c5d3105530ee
AWS CDK Team <aws-cdk@amazon.com>
2023-09-30 11:51:49 +0000

    chore(release): awscdk@v2.99.1  awscdkamplifyalpha@v2.99.1-alpha.0  awscdkapigatewayv2alpha@v2.99.1-alpha.0  awscdkapigatewayv2authorizersalpha@v2.99.1-alpha.0  awscdkapigatewayv2integrationsalpha@v2.99.1-alpha.0  awscdkappconfigalpha@v2.99.1-alpha.0  awscdkapprunneralpha@v2.99.1-alpha.0  awscdkclilibalpha@v2.99.1-alpha.0  awscdkcloud9alpha@v2.99.1-alpha.0  awscdkcodestaralpha@v2.99.1-alpha.0  awscdkcognitoidentitypoolalpha@v2.99.1-alpha.0  awscdkgameliftalpha@v2.99.1-alpha.0  awscdkgluealpha@v2.99.1-alpha.0  awscdkintegtestsalpha@v2.99.1-alpha.0  awscdkiotactionsalpha@v2.99.1-alpha.0  awscdkiotalpha@v2.99.1-alpha.0  awscdkioteventsactionsalpha@v2.99.1-alpha.0  awscdkioteventsalpha@v2.99.1-alpha.0  awscdkivsalpha@v2.99.1-alpha.0  awscdkkinesisanalyticsflinkalpha@v2.99.1-alpha.0  awscdkkinesisfirehosealpha@v2.99.1-alpha.0  awscdkkinesisfirehosedestinationsalpha@v2.99.1-alpha.0  awscdklambdagoalpha@v2.99.1-alpha.0  awscdklambdapythonalpha@v2.99.1-alpha.0  awscdklocationalpha@v2.99.1-alpha.0  awscdkmskalpha@v2.99.1-alpha.0  awscdkneptunealpha@v2.99.1-alpha.0  awscdkredshiftalpha@v2.99.1-alpha.0  awscdkroute53resolveralpha@v2.99.1-alpha.0  awscdks3objectlambdaalpha@v2.99.1-alpha.0  awscdksagemakeralpha@v2.99.1-alpha.0  awscdkscheduleralpha@v2.99.1-alpha.0  awscdkschedulertargetsalpha@v2.99.1-alpha.0  awscdkservicecatalogappregistryalpha@v2.99.1-alpha.0

--- awscdkschedulertargetsalpha/jsii/jsii.go
+++ awscdkschedulertargetsalpha/jsii/jsii.go
@@ -14,7 +14,7 @@ import (
 	constructs           "github.com/aws/constructs-go/constructs/v10/jsii"
 )
 
-//go:embed aws-cdk-aws-scheduler-targets-alpha-2.99.0-alpha.0.tgz
+//go:embed aws-cdk-aws-scheduler-targets-alpha-2.99.1-alpha.0.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -26,5 +26,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@aws-cdk/aws-scheduler-targets-alpha", "2.99.0-alpha.0", tarball)
+	_jsii_.Load("@aws-cdk/aws-scheduler-targets-alpha", "2.99.1-alpha.0", tarball)
 }
