2567948130d7d0f3f10798ee35d0c841ff6727bb
AWS CDK Team <aws-cdk@amazon.com>
2023-11-14 00:48:39 +0000

    chore(release): awscdk@v2.108.0  awscdkamplifyalpha@v2.108.0-alpha.0  awscdkapigatewayv2alpha@v2.108.0-alpha.0  awscdkapigatewayv2authorizersalpha@v2.108.0-alpha.0  awscdkapigatewayv2integrationsalpha@v2.108.0-alpha.0  awscdkappconfigalpha@v2.108.0-alpha.0  awscdkapprunneralpha@v2.108.0-alpha.0  awscdkclilibalpha@v2.108.0-alpha.0  awscdkcloud9alpha@v2.108.0-alpha.0  awscdkcodestaralpha@v2.108.0-alpha.0  awscdkcognitoidentitypoolalpha@v2.108.0-alpha.0  awscdkgameliftalpha@v2.108.0-alpha.0  awscdkgluealpha@v2.108.0-alpha.0  awscdkintegtestsalpha@v2.108.0-alpha.0  awscdkiotactionsalpha@v2.108.0-alpha.0  awscdkiotalpha@v2.108.0-alpha.0  awscdkioteventsactionsalpha@v2.108.0-alpha.0  awscdkioteventsalpha@v2.108.0-alpha.0  awscdkivsalpha@v2.108.0-alpha.0  awscdkkinesisanalyticsflinkalpha@v2.108.0-alpha.0  awscdkkinesisfirehosealpha@v2.108.0-alpha.0  awscdkkinesisfirehosedestinationsalpha@v2.108.0-alpha.0  awscdklambdagoalpha@v2.108.0-alpha.0  awscdklambdapythonalpha@v2.108.0-alpha.0  awscdklocationalpha@v2.108.0-alpha.0  awscdkmskalpha@v2.108.0-alpha.0  awscdkneptunealpha@v2.108.0-alpha.0  awscdkredshiftalpha@v2.108.0-alpha.0  awscdkroute53resolveralpha@v2.108.0-alpha.0  awscdks3objectlambdaalpha@v2.108.0-alpha.0  awscdksagemakeralpha@v2.108.0-alpha.0  awscdkscheduleralpha@v2.108.0-alpha.0  awscdkschedulertargetsalpha@v2.108.0-alpha.0  awscdkservicecatalogappregistryalpha@v2.108.0-alpha.0

--- awscdk/awslambda/Runtime.go
+++ awscdk/awslambda/Runtime.go
@@ -474,6 +474,17 @@ func Runtime_PYTHON_3_11() Runtime {
 	return returns
 }
 
+func Runtime_PYTHON_3_12() Runtime {
+	_init_.Initialize()
+	var returns Runtime
+	_jsii_.StaticGet(
+		"aws-cdk-lib.aws_lambda.Runtime",
+		"PYTHON_3_12",
+		&returns,
+	)
+	return returns
+}
+
 func Runtime_PYTHON_3_6() Runtime {
 	_init_.Initialize()
 	var returns Runtime
