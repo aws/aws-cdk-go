2b0a582f90b21b85e3c35e6b70830d3abcac0503
AWS CDK Team <aws-cdk@amazon.com>
2025-11-25 13:43:46 +0000

    chore(release): appstagingsynthesizeralpha@v2.229.1-alpha.0  awsbedrockagentcorealpha@v2.229.1-alpha.0  awsbedrockalpha@v2.229.1-alpha.0  awscdk@v2.229.1  awscdkamplifyalpha@v2.229.1-alpha.0  awscdkapplicationsignalsalpha@v2.229.1-alpha.0  awscdkapprunneralpha@v2.229.1-alpha.0  awscdkcloud9alpha@v2.229.1-alpha.0  awscdkcodestaralpha@v2.229.1-alpha.0  awscdkeksv2alpha@v2.229.1-alpha.0  awscdkelasticachealpha@v2.229.1-alpha.0  awscdkgameliftalpha@v2.229.1-alpha.0  awscdkgluealpha@v2.229.1-alpha.0  awscdkintegtestsalpha@v2.229.1-alpha.0  awscdkiotactionsalpha@v2.229.1-alpha.0  awscdkiotalpha@v2.229.1-alpha.0  awscdkioteventsactionsalpha@v2.229.1-alpha.0  awscdkioteventsalpha@v2.229.1-alpha.0  awscdkivsalpha@v2.229.1-alpha.0  awscdkkinesisanalyticsflinkalpha@v2.229.1-alpha.0  awscdklambdagoalpha@v2.229.1-alpha.0  awscdklambdapythonalpha@v2.229.1-alpha.0  awscdklocationalpha@v2.229.1-alpha.0  awscdkmixinspreview@v2.229.1-alpha.0  awscdkmskalpha@v2.229.1-alpha.0  awscdkneptunealpha@v2.229.1-alpha.0  awscdkpipesalpha@v2.229.1-alpha.0  awscdkpipesenrichmentsalpha@v2.229.1-alpha.0  awscdkpipessourcesalpha@v2.229.1-alpha.0  awscdkpipestargetsalpha@v2.229.1-alpha.0  awscdkredshiftalpha@v2.229.1-alpha.0  awscdkroute53resolveralpha@v2.229.1-alpha.0  awscdks3objectlambdaalpha@v2.229.1-alpha.0  awscdksagemakeralpha@v2.229.1-alpha.0  awscdkservicecatalogappregistryalpha@v2.229.1-alpha.0  awsec2alpha@v2.229.1-alpha.0  awsimagebuilderalpha@v2.229.1-alpha.0

--- awscdk/jsii/jsii.go
+++ awscdk/jsii/jsii.go
@@ -15,7 +15,7 @@ import (
 	awscdkcloudassemblyschema "github.com/cdklabs/cloud-assembly-schema-go/awscdkcloudassemblyschema/v48/jsii"
 )
 
-//go:embed aws-cdk-lib-2.229.0.tgz
+//go:embed aws-cdk-lib-2.229.1.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -28,5 +28,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("aws-cdk-lib", "2.229.0", tarball)
+	_jsii_.Load("aws-cdk-lib", "2.229.1", tarball)
 }
